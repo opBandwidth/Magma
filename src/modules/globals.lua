cf503998e26f31da286ae7d46cf40f53514f8349
Bandwidth <76101857+opBandwidth@users.noreply.github.com>
2022-01-04 13:07:01 +0300

    Update globals.lua

--- src/modules/globals.lua
+++ src/modules/globals.lua
@@ -2,5 +2,11 @@ getgenv().Magma = {
     ["Teleport Speed"] = 400,
     ["Is Teleporting"] = false,
 
-    ["Click Teleport"] = false
+    ["Click Teleport"] = false,
+
+    ["WalkSpeed"] = 16,
+    ["JumpPower"] = 100,
+    ["Fly"] = false,
+    ["Fly Speed"] = 100,
+    ["Infinite Jump"] = false
 }
