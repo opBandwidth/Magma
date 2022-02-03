dfce23e3429ecb06d3b7dfc1b4de2d8a09caffa6
Bandwidth <76101857+opBandwidth@users.noreply.github.com>
2022-02-03 21:58:12 +0300

    Garbage Collection Removal
    Garbage Collection Removed for Optimization and Performance & Faster Execution.
    Total Cost for Safes, in Local Currency.
    Method Changes.

--- src/modules/utils.lua
+++ src/modules/utils.lua
@@ -11,23 +11,25 @@ Utils.GetLocalVehiclePacket = Utils.Vehicle.GetLocalVehiclePacket
 
 Utils.Notification = require(Game.Notification)
 
+Utils.UI = require(Module.UI)
+
+Utils.DefaultActions = require(Game.DefaultActions)
+
 Utils.PlayerUtils = require(Game.PlayerUtils)
 
 Utils.AlexRagdoll = require(Module.AlexRagdoll)
 Utils.Ragdoll = Utils.AlexRagdoll.Ragdoll
 
-Utils.DefaultActions = require(Game.DefaultActions)
-
-Utils.UI = require(Module.UI)
-
 Utils.MilitaryTurretConsts = require(Game.MilitaryTurret.MilitaryTurretConsts)
+
+Utils.SafesUI = require(Game.SafesUI)
+
+Utils.Format = require(Module.Format)
 
 Utils.RadioUtils = require(Game.Radio.RadioUtils)
 Utils.RadioPlayer = require(Game.Radio.RadioPlayer)
 
 Utils.JetPack = require(Game.JetPack.JetPack)
-
-Utils.SafesUI = require(Game.SafesUI)
 
 Utils.AlexChassis = require(Module.AlexChassis)
 
