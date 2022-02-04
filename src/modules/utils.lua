dc75dd01282e650bd0eb8bd5be74f17a6a092aab
Bandwidth <76101857+opBandwidth@users.noreply.github.com>
2022-02-04 23:53:43 +0300

    Gun Mods
    haha pistol go brrrrrrr

--- src/modules/utils.lua
+++ src/modules/utils.lua
@@ -1,6 +1,7 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Game = ReplicatedStorage.Game
 local Module = ReplicatedStorage.Module
+local Item = Game.Item
 
 local Utils = {}
 
@@ -15,6 +16,8 @@ Utils.UI = require(Module.UI)
 
 Utils.DefaultActions = require(Game.DefaultActions)
 
+Utils.ItemConfig = Game.ItemConfig
+
 Utils.PlayerUtils = require(Game.PlayerUtils)
 
 Utils.AlexRagdoll = require(Module.AlexRagdoll)
@@ -26,10 +29,16 @@ Utils.SafesUI = require(Game.SafesUI)
 
 Utils.Format = require(Module.Format)
 
-Utils.RadioUtils = require(Game.Radio.RadioUtils)
+Utils.RadioUtils = require(Item.RadioUtils)
 Utils.RadioPlayer = require(Game.Radio.RadioPlayer)
 
 Utils.JetPack = require(Game.JetPack.JetPack)
+
+Utils.Gun = require(Item.Gun)
+
+Utils.Flintlock = require(Item.Flintlock)
+
+Utils.RayCast = require(Module.RayCast)
 
 Utils.AlexChassis = require(Module.AlexChassis)
 
